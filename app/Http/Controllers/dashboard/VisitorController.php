4fe839bc9c3e85df0c8e37365332c429da39ba2d
ab-farhan <farhanbokkor@gmail.com>
2021-07-15 18:32:30 +0600

    create visitor controller

---
+++ app/Http/Controllers/dashboard/VisitorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\visitor;
+
+class VisitorController extends Controller
+{
+    public function index(){
+        return view();
+    }
+}
